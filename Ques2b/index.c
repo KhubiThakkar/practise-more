f4c6b35d4e1564eaa6afba636f06dd7cfbc462c7
BootStrap <khubithakkar@gmail.com>
2021-04-04 04:26:16 +0530

    arranged the structure

--- Ques2b/index.c
+++ Ques2b/index.c
@@ -1,36 +1,57 @@
-// C program for the above approach
-
+/**
+ * @file index.c
+ * @author Khubi Thakkar (you@domain.com)
+ * @brief To arrange the data in CSV file in an array of structure
+ * alphabetically
+ * @version 0.1
+ * @date 2021-04-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * @brief The structure is for every row in the CSV file
+ * It has data of Name, Email ID, GitHub Link
+ */
 typedef struct info_t {
     char* name;
     char* email_id;
     char* git_link;
 } info_t;
 
-
+/**
+ * @brief Function used to sort the data according to the values defined inside
+ * 
+ * @param p pointer of void data type
+ * @param q pinter of void data type
+ * @return int 
+ */
 int comparator(const void* p, const void* q)
 {
-    return strcmp(((info_t*)p)->name,
-                  ((info_t*)q)->name);
+    return strcmp(((info_t*)p)->name,((info_t*)q)->name);
 }
 
-
 int main(){
+    int size = 4;
     FILE* fp = fopen("/home/khubithakkar/practise-more/Ques2b/data.csv", "r");
     if(fp == NULL){
         printf("Can't open the file");
     }
     else{
-        info_t *ptr = (info_t *) malloc (4 * sizeof(info_t));
+        /// pointer to the array of structure info_t
+        info_t *ptr = (info_t *) malloc (size * sizeof(info_t));
+
         char buffer[1024];
         int row = 0,column = 0;
         int i = 0;
+
+        // store the data in CSV file in the structure
         while ((fgets(buffer,1024,fp)))
         {
-            /* code */
             column = 0;
             char *value = strtok(buffer,",");
             row++;
@@ -41,16 +62,13 @@ int main(){
             {
                 /* code */
                 if(column == 0){
-                    (ptr+i)->name = value;      // (ptr+i)->name
-                    printf("%s\n",(ptr+i)->name);
+                    (ptr+i)->name = strdup(value);      
                 }
                 if(column == 1){
-                    (ptr+i)->email_id = value;      // (ptr+i)->name
-                    printf("%s\n",(ptr+i)->email_id);
+                    (ptr+i)->email_id = strdup(value);    
                 }
                 if(column == 2){
-                    (ptr+i)->git_link = value;      // (ptr+i)->name
-                    printf("%s\n",(ptr+i)->git_link);
+                    (ptr+i)->git_link = strdup(value);
                 }
                 value = strtok(NULL, ", ");
                 column++;
@@ -62,29 +80,20 @@ int main(){
         }
 
         //Before sorting structure
-        for(int i=0; i<4;i++){
+        for(int i=0; i<size;i++){
             printf("%s %s %s\n",(ptr+i)->name,(ptr+i)->email_id ,(ptr+i)->git_link);
         }
+        /**
+         * @brief Construct a new qsort object
+         * 
+         */
+        qsort(ptr, size, sizeof(info_t), comparator);
 
-
-        qsort(ptr, 4, sizeof(info_t), comparator);
-        // info_t temp;
-        // int loop=0;
-        // while(loop < 4){
-        //     if(strcmp((ptr+loop+1)->name,(ptr+loop)->name)>0){
-        //         temp = *(ptr+loop);
-        //         *(ptr+loop) = *(ptr+loop+1);
-        //         *(ptr+loop+1) = temp;
-        //     }
-
-        // }
-
-
-        // After sorting structure
-        for(int i=0; i<4;i++){
+        // After sorting the structure
+        printf("Alphabetically arranged structure: \n");
+        for(int i=0; i<size;i++){
             printf("%s %s %s\n",(ptr+i)->name,(ptr+i)->email_id ,(ptr+i)->git_link);
         }
- 
     }
 }
 
